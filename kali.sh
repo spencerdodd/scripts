26f1886e58359cadc4c599f41a8cbebec77148aa
spencerdodd <spencer.evan.dodd@gmail.com>
2018-01-01 17:32:38 -0500

    updates

--- kali.sh
+++ kali.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 
-echo 'Updating apt sources.list...'
-echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' > /etc/apt/sources.list
+# Usage:
+#	run once to update apt sources and install guest additions
+#	run again after reboot to complete
 
-echo 'Updating and upgrading dist...'
-apt-get update && apt-get upgrade -y
+step_one="/tmp/kali_upgrade_step_one_complete"
+step_two="/tmp/kali_upgrade_step_two_complete"
 
-echo 'Installing guest additions...'
-apt-get install -y virtualbox-guest-x11
+if [ ! -f $step_one ]; then
+	echo '[*] updating apt sources.list...'
+	echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' > /etc/apt/sources.list
 
-if [ ! -f /tmp/reboot_step ]; then
-	echo 'Rebooting to complete install...'
-	touch /tmp/reboot_step
+	echo '[*] updating and upgrading dist...'
+	apt-get update && apt-get upgrade -y
+
+	echo '[*] installing guest additions...'
+	apt-get install -y virtualbox-guest-x11
+
+	echo '[*] rebooting to complete install...'
+	touch $step_one
 	reboot
+else
+	echo '[+] step one already complete. moving to step two'
 fi
 
-echo 'Upgrading the dist'
-apt-get dist-upgrade
+if [ ! -f $step_two ]; then
+	echo '[*] upgrading the dist'
+	apt-get dist-upgrade
 
-echo 'Rebooting to complete install...'
-reboot
+	echo '[*] rebooting to complete install...'
+	touch $step_two
+	reboot
+else
+	echo '[+] step two already complete'
+fi
+
+echo '[+] guest additions installed!'
